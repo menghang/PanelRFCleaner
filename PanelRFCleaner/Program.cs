8d977da0a118c7052b936c1d1f99e268badd720e
Meng Hang <chocolate.menghang@gmail.com>
2019-01-02 17:08:09 +0800

    Initial commit.
    Signed-off-by: Meng Hang <chocolate.menghang@gmail.com>

--- PanelRFCleaner/Program.cs
+++ PanelRFCleaner/Program.cs
@@ -1,15 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text.RegularExpressions;
 
 namespace PanelRFCleaner
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (FileInfo f in root.GetFiles())
+            {
+                if (f.Extension.Equals(".log"))
+                {
+                    string file = f.FullName;
+                    string newFile = 
+                        Path.Combine(f.DirectoryName, Path.GetFileNameWithoutExtension(f.FullName) + ".csv");
+                    int ii = 1;
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        using (StreamWriter sw = new StreamWriter(newFile))
+                        {
+                            string line = null;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                if (ii == 1 || (!line.Contains("False") && !line.Contains("MeasureNTC")))
+                                {
+                                    string newLine = line.Replace("True>", "").Replace("openRelay", "");
+                                    newLine = Regex.Replace(newLine, @"\[\S+\]", "");
+                                    newLine = Regex.Replace(newLine, @"Wait[0-9]+", "");
+                                    newLine = newLine.Replace("\t", ",");
+                                    sw.WriteLine(newLine);
+                                    ii++;
+                                }
+                            }
+                        }
+                    }
+                    if (ii - 1 == 1)
+                    {
+                        File.Delete(newFile);
+                    }
+                }
+            }
         }
     }
 }
